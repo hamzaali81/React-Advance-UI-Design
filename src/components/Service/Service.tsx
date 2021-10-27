0664c00203d2aa61d3cb3701abb2f9e187fa15a5
hamzaali <hamza54834@gmail.com>
2021-10-27 16:32:15 +0500

    UI Design: Complete

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Service = () => {
-    return (
-        <Container>
-            <Left>
-                <Image />
-            </Left>
-            <Right>
-                <Wrapper></Wrapper>
-            </Right>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Left>
+        <Image />
+      </Left>
+      <Right>
+        <Wrapper>
+          <Title>Simple Process to start</Title>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, omnis
+            quae pariatur accusamus aspernatur alias voluptatem. Enim, ea,
+            nesciunt, odit distinctio esse vero explicabo quo dolore ipsa qui
+            vel tenetur?
+          </p>
+        </Wrapper>
+      </Right>
+    </Container>
+  );
+};
 
 export default Service;
-
 
 const Container = styled.div`
   display: flex;
@@ -32,10 +39,24 @@ const Image = styled.img`
 
 const Right = styled.div`
   width: 50%;
- `;
+`;
 
- const Wrapper = styled.div`
-   padding: 50px;
-   display: flex;
-   flex-direction: column;
-  `;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1``;
+
+const Desc = styled.p`
+  font-size: 20px;
+  margin-top: 20px;
+  color: #555;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`;
